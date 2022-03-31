7b743188066db1d69e85c7e93a14e87960bd8ec9
sang noh <sangyoung123@googlemail.com>
2022-03-31 10:03:00 +0100

    Added rudimentary functions

--- src/SYNJULIA.jl
+++ src/SYNJULIA.jl
@@ -1,3 +1,22 @@
+
+
+
 module SYNJULIA
+
+mutable struct Position
+    x::Int
+    y::Int
+end
+
+struct Size
+    width::Int
+    height::Int
+end
+
+struct Widget
+    width::String
+    position::Position
+end
+
 
 end # module
